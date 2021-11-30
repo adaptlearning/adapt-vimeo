2d180660d0b036a0c5efff824e265621ac4fe671
Paul Steven <paul@mediakitchen.co.uk>
2021-11-30 10:02:25 +0000

    Formatted using 2 spaces

--- js/VimeoView.js
+++ js/VimeoView.js
@@ -2,92 +2,92 @@ import Player from 'libraries/vimeo-player.min';
 
 export default class VimeoView extends Backbone.View {
 
-    get template() {
-        return 'vimeoVideo';
-    }
+  get template() {
+    return 'vimeoVideo';
+  }
 
-    className() {
-        return 'vimeo__video';
-    }
+  className() {
+    return 'vimeo__video';
+  }
 
-    initialize() {
-        this.vimeoEvents = [
-            'play',
-            'pause',
-            'ended',
-            'timeupdate',
-            'seeking',
-            'seeked',
-            'error',
-            'loaded'
-        ];
+  initialize() {
+    this.vimeoEvents = [
+      'play',
+      'pause',
+      'ended',
+      'timeupdate',
+      'seeking',
+      'seeked',
+      'error',
+      'loaded'
+    ];
 
-        this.setupPlayer();
-        this.setupResponsiveSizing();
-        this.setupEventListeners();
-    }
+    this.setupPlayer();
+    this.setupResponsiveSizing();
+    this.setupEventListeners();
+  }
 
-    /**
+  /**
      * Instantiate the vimeo player with the options supplied
      */
-    setupPlayer() {
-        const options = {
-            url: this.model._source.trim(),
-            autoplay: this.model._autoplay,
-            loop: this.model._loop
-        };
+  setupPlayer() {
+    const options = {
+      url: this.model._source.trim(),
+      autoplay: this.model._autoplay,
+      loop: this.model._loop
+    };
 
-        this.player = new Player(this.el, options);
-    }
+    this.player = new Player(this.el, options);
+  }
 
-    /**
+  /**
      * Use the vimeo player's methods to determine the aspect ratio,
      * and set the padding on this view's element accordingly
      */
-    async setupResponsiveSizing() {
-        const videoWidth = await this.player.getVideoWidth();
-        const videoHeight = await this.player.getVideoHeight();
-        this.$el.css('padding-top', `${videoHeight / videoWidth * 100}%`);
-        this.trigger('ready');
-    }
+  async setupResponsiveSizing() {
+    const videoWidth = await this.player.getVideoWidth();
+    const videoHeight = await this.player.getVideoHeight();
+    this.$el.css('padding-top', `${videoHeight / videoWidth * 100}%`);
+    this.trigger('ready');
+  }
 
-    /**
+  /**
      * Trigger the vimeo player's events on this view so that it can act as an abstraction of the player
      * also set up inview listener for 'pause when offscreen', if that's been enabled
      */
-    setupEventListeners() {
-        this.vimeoEvents.forEach(eventType => {
-            this.player.on(eventType, data => {
-                this.trigger(eventType, {
-                    type: eventType,
-                    data: data
-                });
-            });
+  setupEventListeners() {
+    this.vimeoEvents.forEach(eventType => {
+      this.player.on(eventType, data => {
+        this.trigger(eventType, {
+          type: eventType,
+          data: data
         });
+      });
+    });
 
-        if (!this.model._pauseWhenOffScreen) return;
+    if (!this.model._pauseWhenOffScreen) return;
 
-        this.$el.on('inview.pauseWhenOffScreen', this.onPlayerInview.bind(this));
-    }
+    this.$el.on('inview.pauseWhenOffScreen', this.onPlayerInview.bind(this));
+  }
 
-    onPlayerInview(event, isInView) {
-        if (isInView) return;
-        this.player.getPaused().then(paused => !paused && this.player.pause());
-    }
+  onPlayerInview(event, isInView) {
+    if (isInView) return;
+    this.player.getPaused().then(paused => !paused && this.player.pause());
+  }
 
-    /**
+  /**
      * Destroy the player instance before removal
      */
-    remove() {
-        if (this.model._pauseWhenOffScreen) this.$el.off('inview.pauseWhenOffScreen');
+  remove() {
+    if (this.model._pauseWhenOffScreen) this.$el.off('inview.pauseWhenOffScreen');
 
-        try {
-            this.player.destroy();
-        } catch (e) {
-            console.log(e);
-        }
-
-        super.remove();
+    try {
+      this.player.destroy();
+    } catch (e) {
+      console.log(e);
     }
+
+    super.remove();
+  }
 
 }
--- js/adapt-vimeo.js
+++ js/adapt-vimeo.js
@@ -7,130 +7,130 @@ import COMPLETION from './completionEnum';
 
 class Vimeo extends ComponentView {
 
-    events() {
-        return {
-            'click .js-skip-to-transcript': 'onSkipToTranscript'
-        };
-    }
+  events() {
+    return {
+      'click .js-skip-to-transcript': 'onSkipToTranscript'
+    };
+  }
 
-    postRender() {
-        this.$widget = this.$('.component__widget');
-        this.setupPlayer();
-        this.setupTranscript();
-        this.setupEventListeners();
-    }
+  postRender() {
+    this.$widget = this.$('.component__widget');
+    this.setupPlayer();
+    this.setupTranscript();
+    this.setupEventListeners();
+  }
 
-    /**
+  /**
      * Setup the vimeo view and player instance
      */
-    setupPlayer() {
-        this.vimeoView = this.addSubview(VimeoView, this.model.get('_media'));
+  setupPlayer() {
+    this.vimeoView = this.addSubview(VimeoView, this.model.get('_media'));
 
-        this.listenToOnce(this.vimeoView, 'ready', this.setReadyStatus);
-    }
+    this.listenToOnce(this.vimeoView, 'ready', this.setReadyStatus);
+  }
 
-    /**
+  /**
      * Render the transcript and setup the completion event if configured
      */
-    setupTranscript() {
-        const transcriptConfig = this.model.get('_transcript') || {};
+  setupTranscript() {
+    const transcriptConfig = this.model.get('_transcript') || {};
 
-        if (!transcriptConfig._inlineTranscript && !transcriptConfig._externalTranscript) {
-            return;
-        }
-
-        this.transcriptView = this.addSubview(TranscriptView, transcriptConfig);
-
-        if (!transcriptConfig._setCompletionOnView) return;
-        this.listenToOnce(this.transcriptView, 'transcript:open', this.setCompletionStatus);
+    if (!transcriptConfig._inlineTranscript && !transcriptConfig._externalTranscript) {
+      return;
     }
 
-    setupEventListeners() {
-        this.listenTo(this.vimeoView, {
-            play: this.handleMediaEvent,
-            pause: this.handleMediaEvent,
-            ended: this.handleMediaEvent
-        });
+    this.transcriptView = this.addSubview(TranscriptView, transcriptConfig);
 
-        const completionEvent = COMPLETION(this.model.get('_setCompletionOn')) || COMPLETION.PLAY;
+    if (!transcriptConfig._setCompletionOnView) return;
+    this.listenToOnce(this.transcriptView, 'transcript:open', this.setCompletionStatus);
+  }
 
-        if (completionEvent === COMPLETION.INVIEW) {
-            return this.setupInviewCompletion('.component__widget', this.setCompletionStatus);
-        }
+  setupEventListeners() {
+    this.listenTo(this.vimeoView, {
+      play: this.handleMediaEvent,
+      pause: this.handleMediaEvent,
+      ended: this.handleMediaEvent
+    });
 
-        this.listenToOnce(this.vimeoView, completionEvent.asLowerCase, this.setCompletionStatus);
+    const completionEvent = COMPLETION(this.model.get('_setCompletionOn')) || COMPLETION.PLAY;
+
+    if (completionEvent === COMPLETION.INVIEW) {
+      return this.setupInviewCompletion('.component__widget', this.setCompletionStatus);
     }
 
-    /**
+    this.listenToOnce(this.vimeoView, completionEvent.asLowerCase, this.setCompletionStatus);
+  }
+
+  /**
      * Add a sub view to this component's widget element
      * @param {Backbone.View} constructor
      * @param {object} model - the data to pass as the view's model
      * @returns {Backbone.View}
      */
-    addSubview(constructor, model) {
-        const view = new constructor({ model });
-        this.$widget.append(view.$el);
-        return view;
-    }
+  addSubview(constructor, model) {
+    const view = new constructor({ model });
+    this.$widget.append(view.$el);
+    return view;
+  }
 
-    onSkipToTranscript() {
-        Adapt.a11y.focusFirst(this.$('.vimeo__transcript-btn'));
-    }
+  onSkipToTranscript() {
+    Adapt.a11y.focusFirst(this.$('.vimeo__transcript-btn'));
+  }
 
-    /**
+  /**
      * Filter rapidly firing events so that only meaningful 'media' events are triggered
      * @param {object} event
      */
-    handleMediaEvent(event) {
-        const eventType = event.type;
-        const rapidFireTimeoutWindow = 500;
+  handleMediaEvent(event) {
+    const eventType = event.type;
+    const rapidFireTimeoutWindow = 500;
 
-        const currentTime = Date.now();
-        const lastEventTime = this.lastEventTime || 0;
-        this.lastEventTime = currentTime;
+    const currentTime = Date.now();
+    const lastEventTime = this.lastEventTime || 0;
+    this.lastEventTime = currentTime;
 
-        clearTimeout(this.waitingTriggerTimeout);
+    clearTimeout(this.waitingTriggerTimeout);
 
-        const isAlwaysTriggered = (eventType === 'ended');
-        if (isAlwaysTriggered) {
-            return this.triggerGlobalEvent(eventType);
-        }
-
-        const lastEventDuration = (currentTime - lastEventTime);
-        const isRapidlyFiring = (lastEventDuration <= rapidFireTimeoutWindow);
-        if (isRapidlyFiring) return;
-
-        // Wait for rapidFireTimeoutWindow until triggering
-        // Allows trigger to be dropped if another incoming event arrives
-        const trigger = this.triggerGlobalEvent.bind(this, eventType);
-        this.waitingTriggerTimeout = setTimeout(trigger, rapidFireTimeoutWindow);
+    const isAlwaysTriggered = (eventType === 'ended');
+    if (isAlwaysTriggered) {
+      return this.triggerGlobalEvent(eventType);
     }
 
-    /**
+    const lastEventDuration = (currentTime - lastEventTime);
+    const isRapidlyFiring = (lastEventDuration <= rapidFireTimeoutWindow);
+    if (isRapidlyFiring) return;
+
+    // Wait for rapidFireTimeoutWindow until triggering
+    // Allows trigger to be dropped if another incoming event arrives
+    const trigger = this.triggerGlobalEvent.bind(this, eventType);
+    this.waitingTriggerTimeout = setTimeout(trigger, rapidFireTimeoutWindow);
+  }
+
+  /**
      * Trigger a global 'media' event
      * @param {string} eventType
      */
-    triggerGlobalEvent(eventType) {
-        Adapt.trigger('media', {
-            isVideo: true,
-            type: eventType,
-            src: this.model.get('_media')._source.trim(),
-            platform: 'Vimeo'
-        });
-    }
+  triggerGlobalEvent(eventType) {
+    Adapt.trigger('media', {
+      isVideo: true,
+      type: eventType,
+      src: this.model.get('_media')._source.trim(),
+      platform: 'Vimeo'
+    });
+  }
 
-    /**
+  /**
      * Remove this view and sub views. Destroys the vimeo player instance
      */
-    remove() {
-        this.vimeoView.remove();
-        this.transcriptView && this.transcriptView.remove();
-        super.remove();
-    }
+  remove() {
+    this.vimeoView.remove();
+    this.transcriptView && this.transcriptView.remove();
+    super.remove();
+  }
 
 };
 
 export default Adapt.register('vimeo', {
-    model: ComponentModel.extend({}), // create a new class in the inheritance chain so it can be extended per component type if necessary later
-    view: Vimeo
+  model: ComponentModel.extend({}), // create a new class in the inheritance chain so it can be extended per component type if necessary later
+  view: Vimeo
 });
